cf2e9cd71118980607b99024171c66b0ddea6e36
Jr08151 <Jr08151@users.noreply.github.com>
2020-07-16 20:04:07 -0400

    update

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -8,30 +8,30 @@ describe ("Testing our User-Onboard form", function(){
         // cy.pause()
 
         cy
-        .get('input[name="name"]') // grab the input element with the name of name
-        .type("Dino") // type in Dino in it
-        .should('have.value', "Dino") // confirm that Dino is indeed a value inside
+        .get('input[name="name"]') 
+        .type("JR") 
+        .should('have.value', "JR") 
 
         cy
-        .get('input[id="email"]') // grab the element by id
-        .type('TuckerSlimShady@yahoo.com') // type in a certain email
-        .should('have.value','TuckerSlimShady@yahoo.com') // make sure that the email typed matches what's expected
+        .get('input[id="email"]') 
+        .type('JR@nomail.com') 
+        .should('have.value','JR@nomail.com') 
        
         cy
-        .get('input[name="password"]') // grab the element by name again
-        .type("password") // type in password
-        .should('be.visible', "password") // verify that's the password
+        .get('input[name="password"]') 
+        .type("password") 
+        .should('be.visible', "password") 
 
         cy
-        .get('input[type="checkbox"]') // grab the element by type
-        .check()//check the checkbox
-        .should('be.checked') // assure it stays checked 
+        .get('input[type="checkbox"]') 
+        .check()
+        .should('be.checked') 
 
         cy
-        .get('input') // all inputs should not be empty
+        .get('input') 
         .should('be.empty')
         
-        cy.get("button").click() // clicks the button and makes sure it's working on post
+        cy.get("button").click() 
 
 
     })
